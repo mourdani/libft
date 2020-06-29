029ed03964e88e5d939ef29541656a27d2ebb10d
mourdani <57743239+mourdani@users.noreply.github.com>
2020-06-29 23:03:23 +0200

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,15 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Locates the first occurrence of c in the string
+** pointed to by s
+**
+** s: The string to be scanned
+** c: The character to be searched
+**
+** returns: A pointer to the located character
+**          NULL otherwise
+*/
+
 char	*ft_strchr(const char *s, int c)
 {
 	int i;
 
 	i = 0;
 	while (s[i] && s[i] != c)
-	{
 		i++;
-	}
 	if (s[i] != c)
 		return (0);
 	return (char *)(s + i);
